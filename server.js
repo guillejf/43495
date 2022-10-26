62279130d2cc71f1d8a87661939a3343bf310b66
guillejf <maildeguille@gmail.com>
2022-10-25 21:00:28 -0300

    express II

--- server.js
+++ server.js
@@ -1,34 +1,69 @@
-const express = require("express");
-const Contenedor = require("./classContainer");
-const fs = require("fs");
-
+const express = require('express');
 const app = express();
-
 const port = process.env.PORT || 8080;
 
-app.get("/", async (req, res) => {
-  const contenedor = new Contenedor();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-  const todos = await contenedor.getAll();
-  res.json(todos);
+let usuarios = [
+  { id: 100, nombre: 'monica', edad: 20 },
+  { id: 101, nombre: 'jorge', edad: 21 },
+  { id: 102, nombre: 'raul', edad: 22 },
+  { id: 103, nombre: 'juana', edad: 23 },
+];
+
+//GET CON QUERY TIPO SEARCH (OJO QUE ES EL MISMO!)
+app.get('/api/usuarios', (req, res) => {
+  const { query } = req;
+
+  if (query?.nombre) {
+    const usuariosFiltrado = usuarios.filter(
+      (usuario) => usuario.nombre == query.nombre
+    );
+
+    return res.json(usuariosFiltrado);
+  }
+
+  res.json(usuarios);
 });
 
-app.get("/matias", (req, res) => {
-  res.json({ id: 102, name: "mati", age: 40 });
+//POST CON BODY (SIN ID!!)
+app.post('/api/usuarios', (req, res) => {
+  const { body } = req;
+  usuarios.push(body);
+  res.json('ok');
 });
 
-app.get("/random", (req, res) => {
-  res.json({ id: 103, name: "sofia", age: 50 });
+//GET CON ID IDENTIFICADOR EN LA URL TIPO PARAMS
+app.get('/api/usuarios/:id', (req, res) => {
+  const { id } = req.params;
+
+  const usuarioEncontrado = usuarios.find((usuario) => usuario.id == id);
+  if (usuarioEncontrado) {
+    res.json({ success: true, user: usuarioEncontrado });
+  } else {
+    res.json({ error: true, msg: 'no encontrado' });
+  }
 });
 
-app.get("/alumnos", async (req, res) => {
-  const todos = await contenedor.getAll();
-  res.json([
-    { id: 100, name: "carla", age: 20 },
-    { id: 101, name: "guille", age: 30 },
-    { id: 102, name: "mati", age: 40 },
-    { id: 103, name: "sofia", age: 50 },
-  ]);
+//PUT CON ID PARAMS SIEMPRE y BODY!
+app.put('/api/usuarios/:id', (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  const indiceEncontrado = usuarios.findIndex((usuario) => usuario.id == id);
+  if (indiceEncontrado >= 0) {
+    usuarios[indiceEncontrado] = body;
+    res.json({ success: true, user: body });
+  } else {
+    res.json({ error: true, msg: 'no encontrado' });
+  }
+});
+
+//DELETE CON ID PARAMS SIEMPRE
+app.delete('/api/usuarios/:id', (req, res) => {
+  const { id } = req.params;
+  usuarios = usuarios.filter((usuario) => usuario.id != id);
+  res.json({ success: true });
 });
 
 app.listen(port, () => {
